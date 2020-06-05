29d3309288e8a5f2b349050f069db7971dc7f2c0
artem <my@example.com>
2020-06-05 11:02:43 +0300

    update backend

--- src/main/java/com/example/grid_simulator/controller/DiagramController.java
+++ src/main/java/com/example/grid_simulator/controller/DiagramController.java
@@ -3,22 +3,35 @@ package com.example.grid_simulator.controller;
 import com.example.grid_simulator.model.Diagram;
 import com.example.grid_simulator.service.IDiagramService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController()
 @RequestMapping("/v1/api/diagram")
 public class DiagramController {
     @Autowired
-    IDiagramService service;
+    private static IDiagramService service;
 
     @RequestMapping(
             value = "/",
             method = RequestMethod.GET)
     public List<Diagram> find_all(){
         return service.findAll();
+    }
+
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(code = HttpStatus.OK)
+    public Boolean add(@RequestBody Diagram diagram){
+        System.out.println(diagram);
+        return true;
     }
 }
--- src/main/java/com/example/grid_simulator/service/DiagramService.java
+++ src/main/java/com/example/grid_simulator/service/DiagramService.java
@@ -2,9 +2,11 @@ package com.example.grid_simulator.service;
 
 import com.example.grid_simulator.model.Diagram;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class DiagramService implements IDiagramService {
     @Override
     public List<Diagram> findAll() {
