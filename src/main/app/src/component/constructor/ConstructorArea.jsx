515f3c0387bff5c07fc591d6d79ad4d56e91b918
artem <my@example.com>
2020-06-05 11:02:17 +0300

    send diagram from front to back

--- src/main/app/src/component/constructor/ConstructorArea.jsx
+++ src/main/app/src/component/constructor/ConstructorArea.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Lodash from 'lodash';
 import {
 	DiagramWidget,
@@ -43,52 +44,58 @@ class ConstrucrotArea extends React.Component {
 		this.model.addNode(node2);
 		this.model.addLink(link1);
 
-        // custom model
+		// custom model
         
-        // Set the grid size HERE!
+		// Set the grid size HERE!
 		// this.model.setGridSize(50);
         
-        // Set zoom
-        this.model.zoom = 165
+		// Set zoom
+		this.model.zoom = 165
 
 		this.engine.setDiagramModel(this.model);
     }
     
     hadleSerilize = () => {
         const str = JSON.stringify(this.model.serializeDiagram());
-        console.log(this.model.serializeDiagram())
+				console.log(this.model.serializeDiagram())
+				axios.post('http://localhost:8080/v1/api/diagram/', this.model.serializeDiagram())
+				.then((response) => {
+					console.log(response);
+				}, (error) => {
+					console.log(error);
+				});;
     }
 
 	render() {
 		return (
 			<div>
-                <Button color="inherit" onClick={this.hadleSerilize}>serialize</Button>
-                <ConstructorAreaDnd model={{ type: 'out' }} name="Out Node" backgroundColor="hotpink" />
-                <div
-					className="diagram-layer"
-					onDrop={event => {
-						var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
-						var nodesCount = Lodash.keys(this.engine.getDiagramModel().getNodes()).length;
-						var node = null;
-						if (data.type === 'in') {
-							node = new DefaultNodeModel('Node ' + (nodesCount + 1), 'peru');
-							node.addPort(new DefaultPortModel(true, 'in-1', 'In'));
-						} else {
-							node = new DefaultNodeModel('Node ' + (nodesCount + 1), 'hotpink');
-							node.addPort(new DefaultPortModel(false, 'out-1', 'Out'));
-						}
-						var points = this.engine.getRelativeMousePoint(event);
-						node.x = points.x;
-						node.y = points.y;
-						this.engine.getDiagramModel().addNode(node);
-						this.forceUpdate();
-					}}
-					onDragOver={event => {
-						event.preventDefault();
-					}}
-				>
+				<Button color="inherit" onClick={this.hadleSerilize}>serialize</Button>
+				<ConstructorAreaDnd model={{ type: 'out' }} name="Out Node" backgroundColor="hotpink" />
+					<div
+						className="diagram-layer"
+						onDrop={event => {
+							var data = JSON.parse(event.dataTransfer.getData('storm-diagram-node'));
+							var nodesCount = Lodash.keys(this.engine.getDiagramModel().getNodes()).length;
+							var node = null;
+							if (data.type === 'in') {
+								node = new DefaultNodeModel('Node ' + (nodesCount + 1), 'peru');
+								node.addPort(new DefaultPortModel(true, 'in-1', 'In'));
+							} else {
+								node = new DefaultNodeModel('Node ' + (nodesCount + 1), 'hotpink');
+								node.addPort(new DefaultPortModel(false, 'out-1', 'Out'));
+							}
+							var points = this.engine.getRelativeMousePoint(event);
+							node.x = points.x;
+							node.y = points.y;
+							this.engine.getDiagramModel().addNode(node);
+							this.forceUpdate();
+						}}
+						onDragOver={event => {
+							event.preventDefault();
+						}}
+					>
 				    <DiagramWidget diagramEngine={this.engine} />
-                </div>
+				</div>
 			</div>
 		);
 	}
